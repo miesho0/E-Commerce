bc52c188849e744dad7a13cfcc3c8e788be30f49
Mohamed Hesham <mhesham822@gmail.com>
2025-10-24 20:02:28 +0300

    update

--- src/apis/allorders.ts
+++ src/apis/allorders.ts
@@ -1,10 +1,7 @@
 export async function getAllOrders(cartId: string) {
-  const res = await fetch(
-  `https://ecommerce.routemisr.com/api/v1/orders/user/${cartId}?sort=paidAt:desc`,
-  { cache: "no-store" }
-);
-
+  const res = await fetch(`https://ecommerce.routemisr.com/api/v1/orders/user/${cartId}`, {
+    cache: 'no-store'
+  });
   const { data } = await res.json();
   return data;
 }
-
