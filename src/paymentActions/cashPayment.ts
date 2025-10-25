34b98370d99f431788e241b1523234f2afd01f3a
Mohamed Hesham <mhesham822@gmail.com>
2025-10-25 21:33:25 +0300

    update cash payment and online payment

--- src/paymentActions/cashPayment.ts
+++ src/paymentActions/cashPayment.ts
@@ -13,12 +13,21 @@ import axios from "axios";
         throw Error ("Login First")
     }
 
-    const { data } = await axios.post(`https://ecommerce.routemisr.com/api/v1/orders/${id}`, values,{
-    //   shippingAddress: values  
-    // },{
-        headers:{
-            token : token as string
-        }
-    })
+    // const { data } = await axios.post(`https://ecommerce.routemisr.com/api/v1/orders/${id}`, values,{
+    // //   shippingAddress: values  
+    // // },{
+    //     headers:{
+    //         token : token as string
+    //     }
+    // })
+      const { data } = await axios.post(
+    `https://ecommerce.routemisr.com/api/v1/orders/${id}?url=${encodeURIComponent(baseUrl)}`,
+    { shippingAddress: values },
+    {
+      headers: {
+        token: token as string,
+      },
+    }
+  );
     return data
  }
--- src/paymentActions/onlinePayment.ts
+++ src/paymentActions/onlinePayment.ts
@@ -13,12 +13,21 @@ import axios from "axios";
         throw Error ("Login First")
     }
 
-    const { data } = await axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${id}? url=http://localhost:3000`, {
-      shippingAddress: values  
-    },{
-        headers:{
-            token : token as string
-        }
-    })
+    // const { data } = await axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${id}? url=http://localhost:3000`, {
+    //   shippingAddress: values  
+    // },{
+    //     headers:{
+    //         token : token as string
+    //     }
+    // })
+      const { data } = await axios.post(
+    `https://ecommerce.routemisr.com/api/v1/orders/${id}?url=${encodeURIComponent(baseUrl)}`,
+    { shippingAddress: values },
+    {
+      headers: {
+        token: token as string,
+      },
+    }
+  );
     return data
  }
