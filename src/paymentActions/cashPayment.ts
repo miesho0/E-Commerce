c406bdd502f75a451933d32e43447e0a29537bf8
Mohamed Hesham <mhesham822@gmail.com>
2025-10-25 21:42:54 +0300

    update cash payment and online payment

--- src/paymentActions/cashPayment.ts
+++ src/paymentActions/cashPayment.ts
@@ -20,7 +20,18 @@ import axios from "axios";
     //         token : token as string
     //     }
     // })
-      const { data } = await axios.post(
+//       const { data } = await axios.post(
+//     `https://ecommerce.routemisr.com/api/v1/orders/${id}?url=${encodeURIComponent(baseUrl)}`,
+//     { shippingAddress: values },
+//     {
+//       headers: {
+//         token: token as string,
+//       },
+//     }
+//   );
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+
+  const { data } = await axios.post(
     `https://ecommerce.routemisr.com/api/v1/orders/${id}?url=${encodeURIComponent(baseUrl)}`,
     { shippingAddress: values },
     {
